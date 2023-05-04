f72f3b97f5cda6bcfacbeb63bb250b700c25bc06
Chen Geva <65988508+chenwebsites@users.noreply.github.com>
2023-05-04 11:43:39 -0400

    fix: NameObjSorter - cleaner code

--- src/main/NameObjSorter.java
+++ src/main/NameObjSorter.java
@@ -5,32 +5,37 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
+/**
+ * The NameObjSorter is an improved class to sort the List<NameObj> of names as
+ * an object.
+ */
+
 public class NameObjSorter {
 
 	static List<NameObj> unsortedNameList = new ArrayList<>();
 
 	public static List<NameObj> sortNamesObj(List<String> names) {
 		for (String name : names) {
+
 			NameObj nameObj = new NameObj();
+
 			String[] splitedParts = name.split(" ");
-			nameObj.setLastName(splitedParts[splitedParts.length - 1]);
 			String givenNames = name.substring(0, name.lastIndexOf(" "));
+
+			nameObj.setLastName(splitedParts[splitedParts.length - 1]);
 			nameObj.setGivenName(givenNames);
 
 			unsortedNameList.add(nameObj);
 		}
 
-		Comparator<NameObj> byGivenName = (nameObj1, nameObj2) -> {
-			int givenNameCompare = nameObj1.getGivenName().compareTo(nameObj2.getGivenName());
-			if (givenNameCompare != 0) {
-				return givenNameCompare;
-			}
-			return 0;
-		};
-
-		unsortedNameList.sort(byGivenName);
 		Collections.sort(unsortedNameList, new Comparator<NameObj>() {
+			@Override
+			public int compare(NameObj o1, NameObj o2) {
+				return o1.getGivenName().compareTo(o2.getGivenName());
+			}
+		});
 
+		Collections.sort(unsortedNameList, new Comparator<NameObj>() {
 			@Override
 			public int compare(NameObj o1, NameObj o2) {
 				return o1.getLastName().compareTo(o2.getLastName());
